942bd193b0de072ca3e3807bb7b54726bf71a9ab
Johnny Mast <mastjohnny@gmail.com>
2016-08-29 22:11:04 +0200

    Removed .idea

---
+++ examples/example.php
@@ -0,0 +1,64 @@
+<?php
+require 'autoload.php';
+
+use Redbox\Hydrate\Hydrator;
+
+class User {
+    protected $username = '';
+    private $password = '';
+
+    /**
+     * @return string
+     */
+    public function getUsername ()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword ()
+    {
+        return $this->password;
+    }
+}
+
+/**
+ * Method 1
+ */
+$result1 = Hydrator::hydrate(new User())->with(
+    [
+        'username' => 'abc',
+        'password' => 'pass'
+    ]
+);
+
+/**
+ * Method 2
+ */
+$result2 = Hydrate(new User())->with(
+    [
+        'username' => 'abc',
+        'password' => 'pass'
+    ]
+);
+
+/**
+ * Method 3
+ */
+$hydrator = new Hydrator(new User());
+$result3 = $hydrator->with([
+    [
+        'username' => 'abc',
+        'password' => 'pass'
+    ]
+]);
+
+
+/**
+ * Dump what ever you want
+ */
+var_dump($result1);
+//var_dump($result2);
+//var_dump($result3);
